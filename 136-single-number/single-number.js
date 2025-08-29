da37435a1b53dda7f1ac17e2d85b37c7ad7e92ec
Dikshant Verma <60711028+DikshantVerma@users.noreply.github.com>
2025-08-29 16:52:26 +0530

    Time: 1 ms (71.81%) | Memory: 55.2 MB (71.82%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,18 +3,9 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let hash = {};
-    for (i=0; i < nums.length; i++) {
-        if(!hash[nums[i]]) {
-            hash[nums[i]] =1;
-        }
-        else {
-            hash[nums[i]]++;
-        }
+    let xor =0;
+    for(i=0; i<nums.length;i++){
+        xor = xor ^ nums[i];
     }
-    for (i=0; i < nums.length; i++) {
-        if (hash[nums[i]] ==1) {
-            return nums[i];
-        }
-    }
+    return xor;
 };
